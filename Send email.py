c5ab27228125ede7124b3b1dde53f9baae337494
Senthil Kumar Murugan <45646617+Senthilkumarmurugan@users.noreply.github.com>
2021-02-10 13:21:19 +0530

    Create Send email.py

---
+++ Send email.py
@@ -0,0 +1,26 @@
+def sendEmail(receiver_email: str,message: str) -> dict:
+    """ Function to send an email to the created Users in Orange Hrm"""
+
+    Email=config['Email']
+    port = Email['port']
+    smtp_server = Email['smtp_server']
+    sender_email = Email['sender_email']
+    _password = Email['password']
+    
+    msg = MIMEMultipart()
+    
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+    msg['Subject'] = "Orange Hrm credentials"
+    # msg["cc"]=""
+    msg.attach(MIMEText(message,'plain'))
+    
+    context = ssl.create_default_context()
+    try:
+        with smtplib.SMTP(smtp_server, port) as server:
+            server.starttls(context=context)
+            server.login(sender_email, _password)
+            server.sendmail(sender_email, receiver_email, msg.as_string())
+            return {"Success":"Mail sent successfully"}
+    except Exception as e:
+        return {"Error Exception as ":e}
